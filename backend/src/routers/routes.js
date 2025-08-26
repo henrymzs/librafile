7ed5ae0b8535fb51bfc4e15ba9693fdb2478b558
Henry <henrykaua21@gmail.com>
2025-08-25 23:06:52 -0300

    fix: muda nomenclatura das rotas e api

--- backend/src/routers/routes.js
+++ backend/src/routers/routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import LivroController from '../controllers/LivroController.js';
+import LivroController, { livroController } from '../controllers/LivroController.js';
 const router = express.Router();
 
-router.get('/:id', LivroController.getLivroController);
+router.get('/:id', livroController.getLivros);
+
 router.get('/', LivroController.getLivrosController);
+
 router.post('/', LivroController.postLivroController);
 router.patch('/:id', LivroController.patchLivroController);
 router.delete('/:id', LivroController.deleteLivroController);
