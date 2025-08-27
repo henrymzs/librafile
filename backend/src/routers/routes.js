4e9917d533c8c6a92bb1636c2e080fecb4411b2b
Henry <henrykaua21@gmail.com>
2025-08-26 23:05:04 -0300

    feat: comenta routas que serao refatoradas e renomeia antiga funcao de controller

--- backend/src/routers/routes.js
+++ backend/src/routers/routes.js
@@ -1,13 +1,14 @@
 import express from "express";
-import LivroController, { livroController } from '../controllers/LivroController.js';
+import { bookController } from "../controllers/LivroController.js"
 const router = express.Router();
 
-router.get('/:id', livroController.getLivros);
+router.get('/', bookController.searchAllBooks);
+router.get('/:id', bookController.getBook);
 
-router.get('/', LivroController.getLivrosController);
 
+/*
 router.post('/', LivroController.postLivroController);
 router.patch('/:id', LivroController.patchLivroController);
 router.delete('/:id', LivroController.deleteLivroController);
-
+*/
 export default router;
