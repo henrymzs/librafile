21e4c25588a75c9eacbd7dc61a8f1d02312b928b
Henry <henrykaua21@gmail.com>
2025-09-05 00:33:22 -0300

    feat: remove comentarios dos services e utiliza funcao de utils para validar id

--- backend/src/services/BookService.js
+++ backend/src/services/BookService.js
@@ -3,20 +3,26 @@ import { validateId } from "../utils/utils.js";
 
 export const bookService = {
     async getBookById(id) {
+        validateId(id);
+        /**
         if (!id || isNaN(id)) {
             throw new Error('ID inválido');
         }
-        const livro = await bookRepository.findById(id);
-        if (!livro) {
+        */
+        const book = await bookRepository.findById(id);
+        if (!book) {
             throw new Error('Livro não encontrado');
         }
-        return livro;
+        return book;
     },
 
     async deleteBook(id) {
+        validateId(id);
+        /**
         if (!id || isNaN(id)) {
             throw new Error('ID inválido');
         }
+        */
         const book = await bookRepository.delete(id);
         if (!book) {
             throw new Error('Livro não encontrado');
@@ -52,7 +58,7 @@ export const bookService = {
             throw new Error('Disponibilidade do livro só pode ser "Disponivel" ou "Indisponivel".');
         }
 
-        if (newBook.titulo === newBook.autor) {
+        if (newBook.titulo.trim().toLowerCase() === newBook.autor.trim().toLowerCase()) {
             throw new Error('Titulo e autor não podem ser iguais.');
         }
         
@@ -76,43 +82,3 @@ export const bookService = {
         return await bookRepository.save(newBookFormate);
     },
 }
-/** 
-async function getLivroService(id) {
-    id = validateId(id);
-    const livro = await LivroRepository.getLivroRepository(id);
-    return livro ? livro : null;
-}
-
-async function postLivroService(livro) {
-    if (!livro.titulo || !livro.autor || !livro.anoPublicacao) {
-        throw new Error("Dados do livro incompletos. Título, autor e ano são obrigatórios.");
-    }
-    return await LivroRepository.postLivroRepository(livro);
-}
-
-async function updateLivroService(id, livro) {
-    id = validateId(id);
-    if (!livro || Object.keys(livro).length === 0) {
-        throw new Error("Nenhum dado válido para atualização");
-    }
-    const livroAtualizado = await LivroRepository.updateLivroRepository(id, livro);
-    if (!livroAtualizado) {
-        throw new Error("Livro não encontrado");
-    }
-    return livroAtualizado;
-}
-
-async function deleteLivroService(id) {
-    id = validateId(id);
-    const success = await LivroRepository.deleteLivroRepository(id);
-    if (!success) {
-        throw new Error("Livro não encontrado");
-    }
-    return success;
-}
-
-export default { 
-    postLivroService,
-    updateLivroService,
-    deleteLivroService
-  }*/
