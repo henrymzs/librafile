31ecd48574c4dce071ea74fda89e14220cd13698
Henry <henrykaua21@gmail.com>
2025-09-05 00:32:41 -0300

    fix: remove comentarios de repositories

--- backend/src/repositories/BookRepository.js
+++ backend/src/repositories/BookRepository.js
@@ -3,7 +3,7 @@ import { writeFileSync } from 'fs';
 import path from 'path';
 
 let books = [];
-const booksPath = path.resolve("./Bookss.json");
+const booksPath = path.resolve("./Books.json");
 
 export const bookRepository = {
     async findById(id) {
@@ -32,72 +32,8 @@ export const bookRepository = {
     async save({ titulo, autor, anoPublicacao, disponibilidade }) {
         const newBook = new Book(titulo, autor, anoPublicacao, disponibilidade);
         books.push(newBook);
-        writeFileSync(booksPath, JSON.stringify(books, null, 2));
+        writeFileSync(booksPath, JSON.stringify(books, null, 2), 'utf-8');
 
         return newBook;
     }
 };
-/** 
-async function getLivroRepository(id) {
-    return new Promise((resolve, reject) => {
-        return resolve(livros.find(l => l.codigo === id));
-    })
-}
-
-async function getLivrosRepository() {
-    return new Promise((resolve, reject) => {
-        return resolve(livros);
-    })
-}
-
-async function postLivroRepository({ titulo, autor, anoPublicacao }) {
-    return new Promise((resolve, reject) => {
-        const novoLivro = new Livros(titulo, autor, anoPublicacao, true);
-        livros.push(novoLivro);
-        return resolve(novoLivro);
-    });
-}
-
-async function updateLivroRepository(id, newLivro) {
-    return new Promise((resolve, reject) => {
-        const index = livros.findIndex(l => l.codigo === id);
-        if (index >= 0) {
-            if (newLivro.titulo && livros[index].titulo !== newLivro.titulo) {
-                livros[index].titulo = newLivro.titulo;
-            }
-            if (newLivro.autor && livros[index].autor !== newLivro.autor) {
-                livros[index].autor = newLivro.autor
-            }
-            if (newLivro.anoPublicacao && livros[index].anoPublicacao !== newLivro.anoPublicacao) {
-                livros[index].anoPublicacao = newLivro.anoPublicacao;
-            }
-            if (newLivro.disponibilidade !== undefined && livros[index].disponibilidade !== newLivro.disponibilidade) {
-                livros[index].disponibilidade = newLivro.disponibilidade;
-            }
-
-            return resolve(livros[index]);
-        }
-        return reject(new Error('Livro não encontrado'));
-    });
-}
-
-async function deleteLivroRepository(id) {
-    return new Promise((resolve, reject) => {
-        const index = livros.findIndex(l => l.codigo === id);
-        if (index >= 0) {
-            livros.splice(index, 1);
-            return resolve(true);
-        }
-        return reject(false);
-    });
-}
-
-
-export default {
-    
-    getLivrosRepository,
-    postLivroRepository,
-    updateLivroRepository,
-    deleteLivroRepository
-}
-*/
