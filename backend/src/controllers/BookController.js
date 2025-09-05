46f0abe5435139e8365a93c542b3fa21838252f4
Henry <henrykaua21@gmail.com>
2025-09-05 00:31:49 -0300

    fix: muda livros para books, mantendo o padrao

--- backend/src/controllers/BookController.js
+++ backend/src/controllers/BookController.js
@@ -4,8 +4,8 @@ import { bookService } from "../services/BookService.js";
 export const bookController = {
     async searchAllBooks(req, res) {
         try {
-            const livros = await bookRepository.findAllBooks();
-            return res.json(livros);
+            const books = await bookRepository.findAllBooks();
+            return res.json(books);
         } catch (error) {
             return res.status(400).json({ error: error.message });
         }
@@ -34,8 +34,8 @@ export const bookController = {
     async getBook(req, res) {
         try {
             const id = Number(req.params.id);
-            const livro = await bookService.getBookById(id);
-            return res.json(livro);
+            const book = await bookService.getBookById(id);
+            return res.json(book);
         } catch (error) {
             return res.status(400).json({ error: error.message });
         }
@@ -45,72 +45,9 @@ export const bookController = {
         try {
             const response = req.body;                        
             const book = await bookService.createBook(response);
-            return res.status(201).json({message:'Livro Adicionado com sucesso!', book});
+            return res.status(201).json({ message:'Livro Adicionado com sucesso!', book });
         } catch (error) {
             return res.status(400).json({ error: error.message });
         }
     }
 }
-
-/**
-async function getLivroController(req, res) {
-    try {
-        const id = Number(req.params.id);
-        const livro = await LivroService.getLivroService(id);
-        if (livro) {
-            res.json(livro);
-        } else {
-            res.status(404).json({ error: 'Livro não encontrado.' });
-        }
-    } catch (error) {
-        res.status(500).json({
-            error: 'Erro interno do servidor',
-            message: error.message
-        });
-    }
-}
-
-async function getLivrosController(req, res) {
-    try {
-        const livros = await LivroRepository.getLivrosRepository();
-        res.json(livros);
-    } catch (error) {
-        res.status(500).json({
-            error: 'Erro interno do servidor',
-            message: error.message
-        });
-    }
-}
-
-async function postLivroController(req, res) {
-    try {
-        const livro = req.body;
-        const result = await LivroService.postLivroService(livro);
-        res.status(201).json(result);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-}
-
-async function patchLivroController(req, res) {
-    try {
-        const id = Number(req.params.id);
-        const livro = req.body;
-        const result = await LivroService.updateLivroService(id, livro);
-        res.json(result);
-    } catch (error) {
-        res.status(400).json({ error: message });
-    }
-}
-
-async function deleteLivroController(req, res) {
-    try {
-        const id = Number(req.params.id);
-        await LivroService.deleteLivroService(id);
-        res.status(200).json({ message: "Livro removido com sucesso." });
-    } catch (error) {
-        res.status(400).json({ error: 'Livro não foi encontrado ou não existe.' });
-    }
-}
-
- */
