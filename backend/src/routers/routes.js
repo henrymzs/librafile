5ac971e5e89445e4760b98948d04187e155c4669
Henry <henrykaua21@gmail.com>
2025-09-08 21:23:37 -0300

    fix: melhorando legibilidade nos nomes das rotas

--- backend/src/routers/routes.js
+++ backend/src/routers/routes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.get('/books/', bookController.searchAllBooks);
 router.get('/book/:id', bookController.getBook);
+router.get('/books/filter/:availability', bookController.getAvailableBooks);
 router.post('/book/', bookController.createBook);
-router.get('/books/filter/:disponibilidade', bookController.getAvailableBooks);
 router.delete('/book/:id', bookController.deleteBook);
 
 export default router;
