cf961859915306cdda25e9a31176e55a5bf3a10f
Henry <henrykaua21@gmail.com>
2025-09-08 21:25:59 -0300

    fix: melhorando legibilidade no model do livro

--- backend/src/models/Book.js
+++ backend/src/models/Book.js
@@ -1,11 +1,11 @@
 export default class Book {
     static nextId = 1;
 
-    constructor(titulo, autor, anoPublicacao, disponibilidade) {
-        this.codigo = Book.nextId++;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.anoPublicacao = anoPublicacao;
-        this.disponibilidade = disponibilidade
+    constructor(title, author, yearPublication, availability) {
+        this.code = Book.nextId++;
+        this.title = title;
+        this.author = author;
+        this.yearPublication = yearPublication;
+        this.availability = availability
     }
 }
